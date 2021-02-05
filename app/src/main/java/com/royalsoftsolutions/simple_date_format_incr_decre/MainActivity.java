d63065c6283bfa801a3546addff7b752c65073b8
Hiren <rs.hirenrathod@gmail.com>
2021-02-05 09:41:41 +0530

    LAST FILE FOR THIS ....

--- app/src/main/java/com/royalsoftsolutions/simple_date_format_incr_decre/MainActivity.java
+++ app/src/main/java/com/royalsoftsolutions/simple_date_format_incr_decre/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private int x = 1;
-    private int no_Of_Date_Get = 100;
+    private int no_Of_Date_Get = 10;
     ArrayList<String> list;
 
     @Override
@@ -27,25 +27,32 @@ public class MainActivity extends AppCompatActivity {
         DateFormat dateFormat = new SimpleDateFormat ( "dd/MM/yyyy" );
         Date date = new Date (  );
         System.out.println ( "Todays Date:" + dateFormat.format ( date ) );
-        System.out.println ( "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH" );
+        System.out.println ( "HHHHHHHHH----TODAYDATE-----HHHHHHHHHHHHHHHHHHHHHHH" );
 
+        System.out.println ( "###########------START FOR LOOP OUTPUT---######################" );
         for (int j = 0; j < no_Of_Date_Get; j++) {
+            add_Date ( x );
+
             System.out.println ( "Week of Days Are      : " + add_Date ( x ) );
+
+
             list.add(add_Date ( x ));
-            System.out.println ( "###############################################" );
+
+
             x++;
         }
-        System.out.println(list);
+        System.out.println ( "###########------END FOR LOOP OUTPUT---######################" );
 
-        Collections.sort(list);
-        for (String aa: list){
-            System.out.println(aa);
+        System.out.println ( "###########------START ARRAY OUTPUT---######################" );
+        System.out.println(list);
+        System.out.println ( "###########------END ARRAY OUTPUT---######################" );
+
 
            /*  arr[k] = list.split(" ");
-            Toast.makeText(this,"${arr[0]} : ${arr[1]}", Toast.LENGTH_LONG).show();*/
+            Toast.makeText(this,"${arr[0]} : ${arr[1]}", Toast.LENGTH_LONG).show();*//*
 
         }
-
+*/
         /*list.add("Ravi");//Adding object in arraylist
         list.add("Vijay");
         list.add("Ravi");
@@ -53,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static String add_Date(int n) {
-        DateFormat dateFormat = new SimpleDateFormat ( " E -  dd  ",Locale.getDefault ());
+        DateFormat dateFormat = new SimpleDateFormat ( " E -  dd/MM/YYYY  ",Locale.getDefault ());
         Calendar calendar = Calendar.getInstance ( );
         calendar.add ( Calendar.DATE, - n );
         String result = dateFormat.format ( calendar.getTime ( ) );
